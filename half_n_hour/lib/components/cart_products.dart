9b94d84aa28b679159d82e24b1779f0093a67eeb
kunalsahni99 <kunal.sahni99@yahoo.com>
2019-07-06 10:31:05 +0530

    added complete cart features

--- half_n_hour/lib/components/cart_products.dart
+++ half_n_hour/lib/components/cart_products.dart
@@ -1,29 +1,10 @@
+import 'package:HnH/screens/cart.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/foundation.dart' as foundation;
-
 import 'package:provider/provider.dart';
 
-import 'photo.dart';
-
-bool get isIOS => foundation.defaultTargetPlatform == TargetPlatform.iOS;
-
 class CartProducts extends StatefulWidget {
-
-  final String imageUrl;
-  final String title;
-  final int qty;
-  final String price;
-
-  CartProducts({
-    this.title,
-    this.qty,
-    this.price,
-    this.imageUrl
-  });
-
   @override
   _CartProductsState createState() => _CartProductsState();
 }
@@ -31,7 +12,6 @@ class CartProducts extends StatefulWidget {
 class _CartProductsState extends State<CartProducts> {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   String userid;
-
   @override
   void initState() {
     super.initState();
@@ -40,6 +20,18 @@ class _CartProductsState extends State<CartProducts> {
 
   Future<Null> getuid() async {
     final FirebaseUser user = await _auth.currentUser();
+    int sum = 0;
+    var cart = Provider.of<Price>(context);
+    Firestore.instance.collection("cart").document(user.uid).collection("cartItem")
+        .getDocuments().then((snapShot){
+      snapShot.documents.forEach((document){
+        sum += document['price'] * document['qty'];
+        setState(() {
+          cart.totPrice = sum;
+        });
+      });
+    });
+
     setState(() {
       userid = user.uid.toString();
     });
@@ -47,6 +39,8 @@ class _CartProductsState extends State<CartProducts> {
 
   @override
   Widget build(BuildContext context) {
+    var cart = Provider.of<Price>(context);
+
     return StreamBuilder<QuerySnapshot>(
       stream: Firestore.instance
           .collection("cart")
@@ -58,8 +52,9 @@ class _CartProductsState extends State<CartProducts> {
         switch (snapshot.connectionState) {
           case ConnectionState.waiting:
             return Center(child: new CircularProgressIndicator(
-              backgroundColor: Colors.black87,
-              )
+              backgroundColor: Colors.white70,
+              valueColor: new AlwaysStoppedAnimation<Color>(Colors.black87),
+            )
             );
           case ConnectionState.none:
             return Center(child: Text("Your Cart is empty"),);
@@ -67,132 +62,57 @@ class _CartProductsState extends State<CartProducts> {
             return new ListView(
               children:
               snapshot.data.documents.map((DocumentSnapshot document) {
-                return new SingleCartProduct(
-                  cart_prod_name: document['title'],
-                  cart_prod_picture: document['imageUrl'],
-                  cart_prod_price: document['price'],
-                  cart_prod_qty: document['qty'],
-                  cart_prod_id: document['Prod_id'],
+                return Dismissible(
+                  background: Container(
+                    color: Colors.redAccent,
+                    child: Container(
+                      alignment: Alignment.centerLeft,
+                      padding: EdgeInsets.only(left: 30.0),
+                      child: Icon(Icons.delete_sweep,
+                        color: Colors.white,
+                        size: 40.0,
+                      ),
+                    ),
+                  ),
+                  secondaryBackground: Container(
+                    color: Colors.redAccent,
+                    child: Container(
+                      alignment: Alignment.centerRight,
+                      padding: EdgeInsets.only(right: 30.0),
+                      child: Icon(Icons.delete_sweep,
+                        color: Colors.white,
+                        size: 40.0,
+                      ),
+                    ),
+                  ),
+                  key: Key(document['Prod_id'].toString()),
+                  onDismissed: (direction)async{
+                    final FirebaseAuth auth = FirebaseAuth.instance;
+                    final FirebaseUser user = await auth.currentUser();
+
+                    Firestore.instance.collection("cart").document(user.uid).collection("cartItem")
+                        .document(document['Prod_id'].toString()).delete().then((value){
+                      Scaffold.of(context).showSnackBar(SnackBar(
+                        content: Text("Product removed from cart"),
+                      ));
+                    });
+
+                    setState(() {
+                      cart.totPrice = cart.totPrice - (document['price'] * document['qty']);
+                    });
+                  },
+                  child: new SingleCartProduct(
+                    cart_prod_name: document['title'],
+                    cart_prod_picture: document['imageUrl'],
+                    cart_prod_price: document['price'],
+                    cart_prod_qty: document['qty'],
+                    cart_prod_id: document['Prod_id'],
+                  ),
                 );
               }).toList(),
             );
         }
       },
-    );
-  }
-}
-
-class SingleCartProduct extends StatelessWidget {
-  final String cart_prod_name;
-  final String cart_prod_picture;
-  final String cart_prod_price;
-  final int cart_prod_qty;
-  final int cart_prod_id;
-
-  SingleCartProduct({
-    this.cart_prod_name,
-    this.cart_prod_picture,
-    this.cart_prod_price,
-    this.cart_prod_qty,
-    this.cart_prod_id
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0, bottom: 5.0),
-      child: Material(
-        borderRadius: BorderRadius.circular(20.0),
-        elevation: 3.0,
-        child: Container(
-          padding: EdgeInsets.only(left: 5.0, right: 10.0),
-          width: MediaQuery.of(context).size.width - 20.0,
-          height: 150.0,
-          decoration: BoxDecoration(
-            color: Colors.white70,
-            borderRadius: BorderRadius.circular(20.0)
-          ),
-          child: Row(
-            children: <Widget>[
-              SizedBox(width: 10.0),
-              Container(
-                height: 150.0,
-                width: 125.0,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(cart_prod_picture),
-                    fit: BoxFit.contain
-                  )
-                ),
-              ),
-
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.only(left: 10.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      Padding(
-                        padding: const EdgeInsets.only(top: 30.0),
-                        child: Text("₹ " + cart_prod_price,
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18.0
-                          ),
-                        ),
-                      ),
-
-                      Container(
-                        padding: EdgeInsets.only(top: 10.0),
-                        child: Text(cart_prod_name,
-                          overflow: TextOverflow.ellipsis,
-                          style: TextStyle(
-                              color: Colors.black26,
-                              fontSize: 15.0
-                          ),
-                        ),
-                      ),
-
-                      Padding(
-                        padding: const EdgeInsets.only(top: 10.0),
-                        child: Row(
-                          children: <Widget>[
-                            IconButton(
-                              icon: Icon(Icons.remove_circle_outline,
-                                color: Colors.redAccent,
-                              ),
-                              onPressed: (){},
-                            ),
-
-                            Padding(
-                              padding: EdgeInsets.only(left: 10.0),
-                              child: Text(cart_prod_qty.toString(),
-                                style: TextStyle(
-                                    color: Colors.black45
-                                ),
-                              ),
-                            ),
-
-                            Padding(
-                              padding: EdgeInsets.only(left: 10.0),
-                              child: IconButton(
-                                icon: Icon(Icons.add_circle_outline,
-                                  color: Colors.lightGreen,
-                                ),
-                                onPressed: (){},
-                              ),
-                            )
-                          ],
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
     );
   }
 }
--- half_n_hour/lib/screens/cart.dart
+++ half_n_hour/lib/screens/cart.dart
@@ -1,8 +1,10 @@
+import 'package:HnH/components/cart_products.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/foundation.dart'as foundation;
 import 'package:flutter/cupertino.dart';
-
-import 'package:HnH/components/cart_products.dart';
+import 'package:provider/provider.dart';
 
 bool get isIOS => foundation.defaultTargetPlatform == TargetPlatform.iOS;
 
@@ -13,8 +15,12 @@ class Cart extends StatefulWidget {
 }
 
 class _CartState extends State<Cart> {
+
+
   @override
   Widget build(BuildContext context) {
+    var cart = Provider.of<Price>(context);
+
     return Scaffold(
       resizeToAvoidBottomPadding: false,
       appBar: AppBar(
@@ -57,7 +63,7 @@ class _CartState extends State<Cart> {
             Expanded(
               child: ListTile(
                 title: Text('Total'),
-                subtitle: Text('\$230'),
+                subtitle: Text('₹ ${cart.totPrice}'),
               ),
             ),
 
@@ -77,5 +83,188 @@ class _CartState extends State<Cart> {
         ),
       ),
     );
+  }
+}
+
+class SingleCartProduct extends StatefulWidget {
+  final String cart_prod_name;
+  final String cart_prod_picture;
+  final int cart_prod_price;
+  final int cart_prod_qty;
+  final String cart_prod_id;
+
+  SingleCartProduct({
+    this.cart_prod_name,
+    this.cart_prod_picture,
+    this.cart_prod_price,
+    this.cart_prod_qty,
+    this.cart_prod_id
+  });
+
+  @override
+  _SingleCartProductState createState() => _SingleCartProductState();
+}
+
+class _SingleCartProductState extends State<SingleCartProduct> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  int currQty, temp;
+
+  @override
+  void initState() {
+    super.initState();
+    currQty = widget.cart_prod_qty;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var cart = Provider.of<Price>(context);
+
+    return Padding(
+      padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0, bottom: 5.0),
+      child: Material(
+        borderRadius: BorderRadius.circular(20.0),
+        elevation: 3.0,
+        child: Container(
+          padding: EdgeInsets.only(left: 5.0, right: 10.0),
+          width: MediaQuery.of(context).size.width - 20.0,
+          height: 150.0,
+          decoration: BoxDecoration(
+              color: Colors.white70,
+              borderRadius: BorderRadius.circular(20.0)
+          ),
+          child: Row(
+            children: <Widget>[
+              SizedBox(width: 10.0),
+              Container(
+                height: 150.0,
+                width: 125.0,
+                decoration: BoxDecoration(
+                    image: DecorationImage(
+                        image: NetworkImage(widget.cart_prod_picture),
+                        fit: BoxFit.contain
+                    )
+                ),
+              ),
+
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.only(top: 30.0),
+                        child: Text("₹ " + widget.cart_prod_price.toString(),
+                          style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18.0
+                          ),
+                        ),
+                      ),
+
+                      Container(
+                        padding: EdgeInsets.only(top: 10.0),
+                        child: Text(widget.cart_prod_name,
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                              color: Colors.black26,
+                              fontSize: 15.0
+                          ),
+                        ),
+                      ),
+
+                      Padding(
+                        padding: const EdgeInsets.only(top: 10.0),
+                        child: Row(
+                          children: <Widget>[
+                            IconButton(
+                              icon: Icon(Icons.remove_circle_outline,
+                                color: Colors.redAccent,
+                              ),
+                              onPressed: ()async{
+                                final FirebaseUser user = await _auth.currentUser();
+
+                                if (currQty <= 1){
+                                  setState(() {
+                                    Firestore.instance.collection("cart").document(user.uid).collection("cartItem")
+                                        .document(widget.cart_prod_id.toString()).delete();
+                                  });
+                                }
+                                else{
+                                  setState(() {
+                                    currQty--;
+                                  });
+                                }
+                                cart.decrease(widget.cart_prod_price);
+
+                                Firestore.instance.collection("cart").document(user.uid).collection("cartItem")
+                                    .document(widget.cart_prod_id.toString())
+                                    .updateData({'qty': currQty});
+                              },
+                            ),
+
+                            Padding(
+                              padding: EdgeInsets.only(left: 10.0),
+                              child: Text(currQty.toString(),
+                                style: TextStyle(
+                                    color: Colors.black45
+                                ),
+                              ),
+                            ),
+
+                            Padding(
+                              padding: EdgeInsets.only(left: 10.0),
+                              child: IconButton(
+                                icon: Icon(Icons.add_circle_outline,
+                                  color: Colors.lightGreen,
+                                ),
+                                onPressed: ()async{
+                                  final FirebaseUser user = await _auth.currentUser();
+
+                                  if (currQty >= 25){
+                                    Scaffold.of(context).showSnackBar(SnackBar(
+                                      content: Text("You can't add more of this item"),
+                                    ));
+                                  }
+                                  else{
+                                    setState(() {
+                                      currQty++;
+                                      cart.increase(widget.cart_prod_price);
+                                    });
+                                  }
+
+
+                                  Firestore.instance.collection("cart").document(user.uid).collection("cartItem")
+                                      .document(widget.cart_prod_id.toString())
+                                      .updateData({'qty': currQty});
+                                },
+                              ),
+                            )
+                          ],
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class Price extends ChangeNotifier{
+  int totPrice = 0;
+
+  void increase(int price){
+    totPrice += price;
+    notifyListeners();
+  }
+
+  void decrease(int price){
+    totPrice -= price;
+    notifyListeners();
   }
 }
