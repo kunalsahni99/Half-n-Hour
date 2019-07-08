fa2fff3a30b14fe6fce6f3ddbe2906e133bf3b5b
Kunal Sahni <kunal.sahni99@yahoo.com>
2019-07-08 08:49:27 +0530

    Update home_page.dart

--- half_n_hour/lib/screens/home_page.dart
+++ half_n_hour/lib/screens/home_page.dart
@@ -1,4 +1,5 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:HnH/screens/product_details.dart';
+
 import 'package:flutter/material.dart';
 import 'package:carousel_pro/carousel_pro.dart';
 import 'package:flutter/foundation.dart' as foundation;
@@ -6,18 +7,24 @@ import 'package:flutter/cupertino.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
 
 import 'package:HnH/screens/cart.dart';
 import './profile.dart';
-import 'categories.dart';
 import 'maps.dart';
 import 'product_new.dart';
 import 'package:HnH/components/photo.dart';
 
 import 'package:flutter/foundation.dart';
+import '../components//category.dart';
+import '../components//brand.dart';
+import '../components//home_category.dart';
+import 'addprod.dart';
 
 bool get isIOS => foundation.defaultTargetPlatform == TargetPlatform.iOS;
-const String _kGalleryAssetsPackage = 'flutter_gallery_assets';
+
 
 class MyHomePage extends StatefulWidget {
   int totProd = 0;
@@ -28,13 +35,21 @@ class MyHomePage extends StatefulWidget {
 }
 
 class home extends State<MyHomePage> {
-  List list = ['12', '11'];
+
 
   SharedPreferences _preferences;
   String uid, uname = "", email = "", avatar;
   bool hasUID = false, isLoggedIn, loggedwithMail, loggedwithPhone;
   final UserUpdateInfo userUpdateInfo = UserUpdateInfo();
   final FirebaseAuth auth = FirebaseAuth.instance;
+  TextEditingController categoryController = TextEditingController();
+  TextEditingController brandController = TextEditingController();
+  GlobalKey<FormState> _categoryFormKey = GlobalKey();
+
+
+  CategoryService _categoryService = CategoryService();
+  BrandService _brandService=BrandService();
+
 
   @override
   void initState() {
@@ -51,6 +66,12 @@ class home extends State<MyHomePage> {
     loggedwithPhone = _preferences.getBool("LoginPhone") ?? false;
 
     setState(() {
+      Firestore.instance.collection("cart").document(user.uid).collection("cartItem")
+          .getDocuments().then((QuerySnapshot value){
+        setState(() {
+          widget.totProd = value.documents.length;
+        });
+      });
       if (uid != null) {
         // for google sign in
         uname = user.displayName;
@@ -92,13 +113,6 @@ class home extends State<MyHomePage> {
         uname = "Guest User";
         email = "guest@example.com";
       }
-
-      Firestore.instance.collection("cart").document(user.uid).collection("cartItem")
-          .getDocuments().then((QuerySnapshot value){
-        setState(() {
-          widget.totProd = value.documents.length;
-        });
-      });
     });
   }
 
@@ -130,68 +144,45 @@ class home extends State<MyHomePage> {
   List<Photo> photos_popular = <Photo>[
     Photo(
         imageUrl: 'images/veg.jpg',
-        title: 'Fruits',
+
         category: 'Fruits & Vegetables',
-        Prod_id: 1,
-        price: '100',
-        desc:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+
+        ),
     Photo(
         imageUrl: 'images/frozen.jpg',
-        title: 'Peas',
         category: 'Frozen Veg',
-        Prod_id: 2,
-        price: '100',
-        desc:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+
+        ),
     Photo(
         imageUrl: 'images/bev.jpg',
-        title: 'Juice',
+
         category: 'Beverages',
-        Prod_id: 3,
-        price: '100',
-        desc:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+        ),
     Photo(
         imageUrl: 'images/brand_f.jpg',
-        title: 'Grocery',
+
         category: 'Branded Food',
-        Prod_id: 4,
-        price: '100',
-        desc:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+        ),
     Photo(
         imageUrl: 'images/be.jpg',
-        title: 'Cosmetics',
+
         category: 'Beauty & Personal Care',
-        Prod_id: 5,
-        price: '100',
-        desc:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+       ),
     Photo(
         imageUrl: 'images/home.jpg',
         title: 'Furniture',
         category: 'Home Care & Fashion',
-        Prod_id: 6,
-        price: '100',
-        desc:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+       ),
     Photo(
         imageUrl: 'images/nonveg.jpg',
-        title: 'Chicken',
+
         category: 'Non Veg',
-        Prod_id: 7,
-        price: '100',
-        desc:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+       ),
     Photo(
         imageUrl: 'images/eggs.jpg',
-        title: 'Cake',
+
         category: 'Dairy, Bakery & Eggs',
-        Prod_id: 8,
-        price: '100',
-        desc:
-            "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."),
+       ),
   ];
 
   @override
@@ -243,6 +234,53 @@ class home extends State<MyHomePage> {
                   );
                 },
               ),
+              new Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: new Container(
+                  height: 150.0,
+                  width: 30.0,
+                  child: new GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(new MaterialPageRoute(
+                          builder: (BuildContext context) => Cart()));
+                    },
+                    child: Stack(
+                      children: <Widget>[
+                        new IconButton(
+                            icon: new Icon(
+                              Icons.shopping_cart,
+                              color: Colors.black87,
+                            ),
+                            onPressed: () {
+                              Navigator.of(context).push(new MaterialPageRoute(
+                                  builder: (BuildContext context) => Cart()));
+                            }),
+                        widget.totProd == 0
+                            ? new Container()
+                            : new Positioned(
+                            child: new Stack(
+                              children: <Widget>[
+                                new Icon(Icons.brightness_1,
+                                    size: 20.0, color: Colors.orange.shade500),
+                                new Positioned(
+                                    top: 3.0,
+                                    right: 5.5,
+                                    child: new Center(
+                                      child: new Text(
+                                        widget.totProd.toString(),
+                                        style: new TextStyle(
+                                            color: Colors.white,
+                                            fontSize: 11.0,
+                                            fontWeight: FontWeight.w500),
+                                      ),
+                                    )),
+                              ],
+                            )),
+                      ],
+                    ),
+                  ),
+                ),
+              )
             ],
           ),
           drawer: Drawer(
@@ -267,9 +305,7 @@ class home extends State<MyHomePage> {
                       child: ClipOval(
                         child: CachedNetworkImage(
                           placeholder: (context, val) =>
-                              CircularProgressIndicator(
-                                valueColor: AlwaysStoppedAnimation<Color>(Colors.black87),
-                              ),
+                              CircularProgressIndicator(),
                           imageUrl: avatar != null
                               ? avatar
                               : "https://cdn4.iconfinder.com/data/icons/avatars-gray/500/avatar-12-512.png",
@@ -297,6 +333,28 @@ class home extends State<MyHomePage> {
                       title: Text('My Orders'),
                       leading: Icon(
                         Icons.shopping_basket,
+                        color: Colors.black87,
+                      )),
+                ),
+                InkWell(
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (_) => AddProduct()));
+
+
+                  },
+                  child: ListTile(
+                      title: Text('Add products'),
+                      leading: Icon(
+                        Icons.add_circle,
+                        color: Colors.black87,
+                      )),
+                ),
+                InkWell(
+                  onTap: () {_categoryAlert();},
+                  child: ListTile(
+                      title: Text('Add category'),
+                      leading: Icon(
+                        Icons.add_circle,
                         color: Colors.black87,
                       )),
                 ),
@@ -350,63 +408,16 @@ class home extends State<MyHomePage> {
               ],
             ),
           ),
-
-          floatingActionButton: FloatingActionButton(
-            onPressed: (){
-              Navigator.push(context, FadeRouteBuilder(page: Cart()));
-            },
-            child: Stack(
-              children: <Widget>[
-                Center(
-                  child: new IconButton(
-                      icon: new Icon(
-                        Icons.shopping_cart,
-                        color: Colors.white,
-                      ),
-                      onPressed: (){
-                        Navigator.of(context).push(
-                            new MaterialPageRoute(
-                                builder:(BuildContext context) =>
-                                    Cart()
-                            )
-                        );
-                      }),
-                ),
-                widget.totProd == 0
-                    ? new Container()
-                    : new Positioned(
-                    child: new Stack(
-                      children: <Widget>[
-                        new Icon(Icons.brightness_1,
-                            size: 20.0, color: Colors.orange.shade500),
-                        new Positioned(
-                            top: 3.0,
-                            right: 5.5,
-                            child: new Center(
-                              child: new Text(
-                                widget.totProd.toString(),
-                                style: new TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 11.0,
-                                    fontWeight: FontWeight.w500),
-                              ),
-                            )),
-                      ],
-                    )),
-              ],
-            ),
-            backgroundColor: Colors.black87,
-          ),
-
           body: new SingleChildScrollView(
             child: Container(
               child: new Column(children: <Widget>[
+                _verticalD(),
                 image_carousel,
                 new Container(
                   margin: EdgeInsets.only(top: 7.0),
                   child: new Row(
                       mainAxisSize: MainAxisSize.max,
-                      mainAxisAlignment: MainAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                       children: <Widget>[
                         _verticalD(),
                         new GestureDetector(
@@ -455,10 +466,18 @@ class home extends State<MyHomePage> {
                               crossAxisCount: 2),
                       itemBuilder: (BuildContext context, int index) {
                         return new GestureDetector(
-                            onTap: (){
-                              Navigator.push(context, MaterialPageRoute(
-                                builder: (context) => CategoriesList(title: photos_popular[index].category,)
-                              ));
+                            onTap: () {
+
+
+                              Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => HomeCategory(
+
+
+                                        category: photos_popular[index].category,
+
+                                      )));
                             },
                             child: new Container(
                                 margin: EdgeInsets.all(5.0),
@@ -501,6 +520,39 @@ class home extends State<MyHomePage> {
   }
 
   _verticalD() => Container(
-        margin: EdgeInsets.only(left: 10.0, right: 5.0, top: 10.0, bottom: 0.0),
+        margin: EdgeInsets.only(left: 5.0, right: 0.0, top: 10.0, bottom: 0.0),
       );
+  void _categoryAlert() {
+    var alert = new AlertDialog(
+      content: Form(
+        key: _categoryFormKey,
+        child: TextFormField(
+          controller: categoryController,
+          validator: (value){
+            if(value.isEmpty){
+              return 'category cannot be empty';
+            }
+          },
+          decoration: InputDecoration(
+              hintText: "add category"
+          ),
+        ),
+      ),
+      actions: <Widget>[
+        FlatButton(onPressed: (){
+          if(categoryController.text != null){
+            _categoryService.createCategory(categoryController.text);
+          }
+          Fluttertoast.showToast(msg: 'category created');
+          Navigator.pop(context);
+        }, child: Text('ADD')),
+        FlatButton(onPressed: (){
+          Navigator.pop(context);
+        }, child: Text('CANCEL')),
+
+      ],
+    );
+
+    showDialog(context: context, builder: (_) => alert);
+  }
 }
