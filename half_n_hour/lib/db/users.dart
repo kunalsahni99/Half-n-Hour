4ed2fe40dab5679d4edc33267f6dae8cf8dac159
Kunal Sahni <kunal.sahni99@yahoo.com>
2019-08-12 20:34:48 +0530

    added ShowDialog()

--- half_n_hour/lib/db/users.dart
+++ half_n_hour/lib/db/users.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_database/firebase_database.dart';
+import 'package:flutter/material.dart';
 
 class UserServices{
   FirebaseDatabase _database = FirebaseDatabase.instance;
@@ -12,5 +13,31 @@ class UserServices{
         .catchError((e){
           print(e.toString());
         });
+  }
+
+  Future<Widget> ShowDialog(BuildContext context){
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (context){
+        return AlertDialog(
+          content: Row(
+            children: <Widget>[
+              CircularProgressIndicator(
+                  valueColor: AlwaysStoppedAnimation<Color>(Colors.black87)
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20.0),
+                child: Text('Please wait...',
+                  style: TextStyle(
+                    color: Colors.black87,
+                  ),
+                ),
+              )
+            ],
+          ),
+        );
+      },
+    );
   }
 }
