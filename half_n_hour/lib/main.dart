336bfa9d7e0a8429048bdbffe20bb9591588aee0
Kunal Sahni <kunal.sahni99@yahoo.com>
2019-08-16 16:23:09 +0530

    updated main.dart

--- half_n_hour/lib/main.dart
+++ half_n_hour/lib/main.dart
@@ -8,14 +8,27 @@ import 'package:flutter/foundation.dart' as foundation;
 import 'package:provider/provider.dart';
 
 import './screens/home_page.dart';
+import './screens/check_out.dart';
 import './screens/onboard.dart';
 import 'package:flutter/rendering.dart';
-
-import 'components/photo.dart';
+import 'package:rect_getter/rect_getter.dart';
 
 bool get isIOS => foundation.defaultTargetPlatform == TargetPlatform.iOS;
 
 void main() => runApp(HalfnHour());
+
+class FadeRouteBuilder<T> extends PageRouteBuilder<T>{
+  final Widget page;
+
+  FadeRouteBuilder({@required this.page})
+      : super(
+      transitionDuration: Duration(milliseconds: 1000),
+      pageBuilder: (context, anim1, anim2) => page,
+      transitionsBuilder: (context, a1, a2, child){
+        return FadeTransition(opacity: a1 , child: child);
+      }
+  );
+}
 
 class HalfnHour extends StatelessWidget {
   @override
@@ -26,6 +39,9 @@ class HalfnHour extends StatelessWidget {
           CupertinoApp(
             debugShowCheckedModeBanner: false,
             title: 'Half n Hour',
+            routes: {
+              '/home': (_) => MyHomePage()
+            },
             theme: CupertinoThemeData(
               primaryColor: Colors.white70,
               barBackgroundColor: Colors.white70
@@ -38,8 +54,11 @@ class HalfnHour extends StatelessWidget {
             theme: ThemeData(
               primarySwatch: Colors.blue,
               primaryColor: Colors.white,
-              fontFamily: 'Product Sans'
+              fontFamily: 'Product Sans',
             ),
+            routes: {
+              '/home': (_) => MyHomePage()
+            },
             home: SplashScreen()
           ),
     );
@@ -47,7 +66,6 @@ class HalfnHour extends StatelessWidget {
 }
 
 class SplashScreen extends StatefulWidget {
-
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
@@ -57,11 +75,15 @@ class _SplashScreenState extends State<SplashScreen> {
   SharedPreferences sharedPreferences;
   bool loading = false,
       isLoggedIn, SignupEmail, isLoggedInEmail, isLoggedInPhone;
+  GlobalKey rectGetterKey = RectGetter.createGlobalKey();
+  final Duration animationDuration = Duration(milliseconds: 300);
+
+  Rect rect;
 
   void initState(){
     super.initState();
     Timer(
-      Duration(seconds: 2),
+      Duration(seconds: 5),
       (){
         isSignIn();
       }
@@ -79,25 +101,10 @@ class _SplashScreenState extends State<SplashScreen> {
     isLoggedInPhone = sharedPreferences.getBool("loggedwithPhone") ?? false;
 
     if (isLoggedIn || isLoggedInEmail || SignupEmail || isLoggedInPhone) {
-      if (isIOS){
-        Navigator.pushReplacement(context, CupertinoPageRoute(
-          builder: (context) => MyHomePage()
-        ));
-      }
-      else{
-        Navigator.pushReplacement(context, MaterialPageRoute(
-            builder: (context) => MyHomePage()));
-      }
+      _onTimeOut(MyHomePage());
     }
     else{
-      if (isIOS){
-        Navigator.pushReplacement(context, CupertinoPageRoute(
-            builder: (context) => OnBoarding()));
-      }
-      else{
-        Navigator.pushReplacement(context, MaterialPageRoute(
-            builder: (context) => OnBoarding()));
-      }
+      _onTimeOut(OnBoarding());
     }
 
     setState(() {
@@ -105,147 +112,114 @@ class _SplashScreenState extends State<SplashScreen> {
     });
   }
 
+  Widget _ripple(){
+    if (rect == null){
+      return Container();
+    }
+    return AnimatedPositioned(
+      duration: animationDuration,
+      left: rect.left,
+      right: MediaQuery.of(context).size.width - rect.right,
+      top: rect.top,
+      bottom: MediaQuery.of(context).size.height - rect.bottom,
+      child: Container(
+        decoration: BoxDecoration(
+          shape: BoxShape.circle,
+            color: Colors.white,
+        ),
+      ),
+    );
+  }
+
+  void _onTimeOut(Widget page) async{
+    setState(() => rect = RectGetter.getRectFromKey(rectGetterKey));
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() =>
+      rect = rect.inflate(1.3 * MediaQuery.of(context).size.longestSide));
+      Future.delayed(animationDuration, () {_goToNextPage(page);});
+    });
+  }
+
+  void _goToNextPage(Widget page){
+    Navigator.pushReplacement(context, FadeRouteBuilder(page: page))
+        .then((_) => setState(() => rect = null));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return isIOS ?
-        CupertinoPageScaffold(
-          child: Stack(
-            fit: StackFit.expand,
-            children: <Widget>[
-              Container(
-                decoration: BoxDecoration(
-                    color: Color(0xff622F74),
-                    gradient: LinearGradient(
-                        colors: [Color(0xff6094e8), Color(0xffde5cbc)],
-                        begin: Alignment.centerRight,
-                        end: Alignment.centerLeft
-                    )
-                ),
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: <Widget>[
-                  Expanded(
-                    flex: 2,
-                    child: Container(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: <Widget>[
-                          CircleAvatar(
-                              backgroundColor: Colors.white,
-                              radius: 50.0,
-                              child: Image.asset('images/launcher_icon.png',
-                                fit: BoxFit.cover,
-                                height: 60.0,
-                                width: 60.0,
-                              )
-                          ),
+    Size size = MediaQuery.of(context).size;
 
-                          Padding(
-                            padding: EdgeInsets.only(top: 10.0),
-                          ),
+    return Stack(
+      children: <Widget>[
+        Scaffold(
+           body: Stack(
+             children: <Widget>[
+               Container(
+                 color: Colors.deepOrange,
+               ),
 
-                          Text('Half n Hour',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 24.0,
-                                fontWeight: FontWeight.bold
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
+               Padding(
+                 padding: const EdgeInsets.only(top: 100.0),
+                 child: Center(
+                   child: Column(
+                     children: <Widget>[
+                       RectGetter(
+                         key: rectGetterKey,
+                         child: Image.asset('images/main_icon.png',
+                           width: 300.0,
+                           height: 300.0,
+                         ),
+                       ),
 
-                  Expanded(
-                    flex: 1,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        Text('Online Store\nfor Everyone',
-                          style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 18.0,
-                              fontWeight: FontWeight.bold
-                          ),
-                        )
-                      ],
-                    ),
-                  )
-                ],
-              )
-            ],
-          ),
-        )
+                       Text('Half n Hour',
+                         style: TextStyle(
+                           color: Colors.white,
+                           fontSize: 30.0,
+                           fontWeight: FontWeight.bold
+                         ),
+                       ),
+                       
+                       Padding(
+                         padding: EdgeInsets.only(top: 80.0),
+                         child: GridTile(
+                           child: Container(
+                             width: double.infinity,
+                             height: 50.0,
+                             color: Colors.white,
+                             child: Padding(
+                               padding: const EdgeInsets.only(top: 10.0),
+                               child: Text('A DOORSTEP SERVICE PROVIDER',
+                                 textAlign: TextAlign.center,
+                                 style: TextStyle(
+                                     color: Colors.deepOrange,
+                                     fontWeight: FontWeight.bold,
+                                     fontSize: 20.0
+                                 ),
+                               ),
+                             ),
+                           ),
+                         )
+                       ),
 
-        : Scaffold(
-      body: Stack(
-        fit: StackFit.expand,
-        children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-                color: Color(0xff622F74),
-                gradient: LinearGradient(
-                    colors: [Color(0xff6094e8), Color(0xffde5cbc)],
-                    begin: Alignment.centerRight,
-                    end: Alignment.centerLeft
-                )
-            ),
-          ),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Expanded(
-                flex: 2,
-                child: Container(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      CircleAvatar(
-                          backgroundColor: Colors.white,
-                          radius: 50.0,
-                          child: Image.asset('images/launcher_icon.png',
-                            fit: BoxFit.cover,
-                            height: 60.0,
-                            width: 60.0,
-                          )
-                      ),
-
-                      Padding(
-                        padding: EdgeInsets.only(top: 10.0),
-                      ),
-
-                      Text('Half n Hour',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 24.0,
-                            fontWeight: FontWeight.bold
-                        ),
-                      )
-                    ],
-                  ),
-                ),
-              ),
-
-              Expanded(
-                flex: 1,
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text('Online Store\nfor Everyone',
-                      style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18.0,
-                          fontWeight: FontWeight.bold
-                      ),
-                    )
-                  ],
-                ),
-              )
-            ],
-          )
-        ],
-      ),
+                       Padding(
+                         padding: EdgeInsets.only(top: 20.0),
+                         child: Text('Anytime Anywhere Anything',
+                           style: TextStyle(
+                               color: Colors.white,
+                               fontWeight: FontWeight.bold,
+                               fontSize: 20.0
+                           ),
+                         ),
+                       ),
+                     ],
+                   ),
+                 ),
+               ),
+             ],
+           )
+        ),
+        _ripple()
+      ],
     );
   }
 }
